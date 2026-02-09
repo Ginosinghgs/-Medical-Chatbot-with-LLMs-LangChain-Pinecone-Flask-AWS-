504cf64c0911d30e6a8c08b5a97cba176237a8b2
Ginosinghgs <ginosingh10@gmail.com>
2026-02-09 22:48:12 +0530

    folder structure created

---
+++ template.sh
@@ -0,0 +1,17 @@
+# creating directories for the project
+mkdir -p src 
+mkdir -p research
+
+
+# creating files for the project
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+TOUCH setup.py
+touch app.py
+touch research/trials.ipynb 
+touch requirements.txt
+
+
+echo "Project structure created successfully!"
